e9d11ad2f1ad07671af19ffa9b3e4418be21d920
BytesCrafter <bytescrafter@gmail.com>
2020-07-31 21:07:34 +0800

    Initially QA signup but with TODO comments.

--- includes/api/v1/users/class-signup.php
+++ includes/api/v1/users/class-signup.php
@@ -11,162 +11,150 @@
 	*/
 ?>
 <?php
-    class DV_Signup{
-        public static function initialize(){
+    class DV_Signup {
 
-            // Initialize WP global variable
-            global $wpdb;
+        public static function listen(){
 
-            // Step1 : check of fields are past
-            if( !isset($_POST['un']) && !isset($_POST['em']) && !isset($_POST['fn']) && !isset($_POST['ln']) && !isset($_POST['gd']) && !isset($_POST['pr']) && !isset($_POST['ct']) ){
+            // Step 1 : Check if the fields are passed
+            if( !isset($_POST['un']) || !isset($_POST['em']) || 
+                !isset($_POST['fn']) || !isset($_POST['ln']) || 
+                !isset($_POST['gd']) || !isset($_POST['bd']) || 
+                !isset($_POST['co']) || !isset($_POST['pr']) || 
+                !isset($_POST['ct']) || !isset($_POST['bg']) ){
                 return rest_ensure_response( 
                     array(
                             "status" => "unknown",
-                            "message" => "Please contact your administrator. Verification Unknown!",
+                            "message" => "Please contact your administrator. Request Unknown!",
                     )
                 );
-                
             }
 
-            // Sanitaion
-            $username = sanitize_user($_POST['un']);
-
-            $user_email = sanitize_email($_POST['em']);
-
-            $user_meta = array(
-                'first_name' => $_POST['fn'],
-                'last_name' => $_POST['ln'],
-            );
-
-            $gender = $_POST['gd'];
-
-            // step1 : check if user exist
-            if ( username_exists( $username ) ) {
+            // Step 2 : Check if username or email is existing.
+            if ( empty($_POST['un']) || empty($_POST['em'])
+                || empty($_POST['fn']) || empty($_POST['ln'])
+                || empty($_POST['gd']) || empty($_POST['bd'])
+                || empty($_POST['co']) || empty($_POST['pr']) 
+                || empty($_POST['ct']) || empty($_POST['bg']) ) {
                 return rest_ensure_response( 
                     array(
-                            "status" => "unknown",
-                            "message" => "Username Already Exists",
+                            "status" => "failed",
+                            "message" => "Required field cannot be empty.",
                     )
                 );
+            }
 
-            } else {
-                // step2 : check call user create function from global
-                $user_result =  DV_Signup::user_create($username,  $user_email);
+            // Step 2 : Check if username or email is existing.
+            if ( username_exists( $_POST['un'] ) ||  email_exists($_POST['em']) ) {
+                return rest_ensure_response( 
+                    array(
+                            "status" => "failed",
+                            "message" => "Username or Email already exist.",
+                    )
+                );
+            }
 
-                // step3 : check if user creation false
-                if ($user_result == false) {
-    
+            //TODO: Check gd, bd, co, pr, ct has a proper value.
+
+            // Step 3 : Actual creation of user.
+            // Initialize WordPress Core DB.
+            global $wpdb;
+
+            // Get user object.
+            $user = DV_Signup::catch_post();
+
+            // Try to create a User.
+            $created_id = wp_insert_user( $user );
+
+            // Handle user creation events.
+            if( !is_wp_error($created_id) ) {
+
+                // Insert Gender etc. 
+                $add_key_meta = update_user_meta( $user_id->id, 'gender', $user['gender'] );
+                $add_key_meta = update_user_meta( $user_id->id, 'birthday', $user['birthday'] );
+
+                // TODO: Insert Address. 
+                // $add_key_meta = update_user_meta( $user_id->id, 'brgy_code', $user['br'] );
+                // $add_key_meta = update_user_meta( $user_id->id, 'city_code', $user['ct'] );
+                // $add_key_meta = update_user_meta( $user_id->id, 'province_code', $user['pv'] );
+                // $add_key_meta = update_user_meta( $user_id->id, 'country', $user['co'] );
+                $add_key_meta = update_user_meta( $user_id->id, 'address_home', "ID of address" );                
+
+                // Insert user meta for expiration of current activation_key.
+                $expiration_date = date( 'Y-m-d H:i:s', strtotime("now") + 1800 ); // seconds = 30 minutes
+                $add_key_meta = update_user_meta( $user_id->id, 'reset_pword_expiry', $expiration_date );
+
+                // Update the user activation key.
+                $wpdb->get_row("UPDATE {$wpdb->prefix}users 
+                    SET `user_activation_key` = {$user['user_activation_key']} 
+                    WHERE `ID` = {$created_id};");
+                
+                // Try to send mail.
+                if( DV_Signup::is_success_sendmail( $user ) ) {
                     return rest_ensure_response( 
                         array(
-                                "status" => "unknown",
-                                "message" => "Please contact your administrator. User Creation Unknown!",
+                                "status" => "success",
+                                "message" => "Please check your email for password reset key.",
                         )
                     );
-                    
-                }else{
-
-		        	// Initialize WP global variable
-                    global $wpdb;
-                    
-                   // step4 : Fetch id from wp_users table
-                    $result = $wpdb->get_row("SELECT id
-                        FROM {$wpdb->prefix}users 
-                        WHERE user_login = '$username' AND user_email = '$user_email' ", OBJECT );
-                    
-                   // step5 : Create WP User data 
-                    $user = new WP_User( (int) $result->id );
-
-                    // step6 : Update user_meta table  
-                    foreach ($user_meta as $key => $value) {
-
-                        $update_meta = update_user_meta($result->id, $key, $value  );
-                       
-                    }
-                    
-                    $add_key_meta_gender = add_user_meta( $result->id, 'gender', $gender );
-
-
-                    // step7 : Create password rest key for User Account Activation   
-                    $adt_rp_key = get_password_reset_key( $user );
-
-                    // important to sending mail activation
-                    $user_login = $user->user_login;
-                    
-                    // step8 : Check if Account Activation key is False   
-                    if ( is_wp_error( $adt_rp_key ) ) {
-                        return rest_ensure_response( 
-                            array(
-                                    "status" => "unknown",
-                                    "message" => "Please contact your administrator. User Authentication key Creation Unknown!",
-                            )
-                        );
-
-                    }else{
-                    
-                         // step9 : Activate send mail function   
-                        $send_email = DV_Signup::send_mail_activation($result, $adt_rp_key, $user_login);
-                        
-                         // step9 : Fetch if send mail function come true   
-                        if ($send_email == false) {
-                            return rest_ensure_response( 
-                                array(
-                                        "status" => "unknown",
-                                        "message" => "Please contact your administrator. User Creation Unknown!",
-                                )
-                            );
-
-                        }else{
-                            return rest_ensure_response( 
-                                array(
-                                        "status" => "Success",
-                                        "message" => "Please check your email account for activation.",
-                                )
-                            );
-
-                        }
-
-                    }
-                   
-                }    
+                } else {
+                    return rest_ensure_response( 
+                        array(
+                                "status" => "failed",
+                                "message" => "Please contact site administrator. Email not sent!",
+                        )
+                    );
+                }
                 
+            } else {
+                return rest_ensure_response( 
+                    array(
+                            "status" => "error",
+                            "message" => "WordPress Error!",
+                    )
+                );
             }
-            
         }
 
-        //Sending email for account verification
-        public static function send_mail_activation($user_id, $adt_rp_key, $user_login){
-            global $wpdb;
+        // Return of SignUp user object from POST.
+        public static function catch_post()
+        {
+            $cur_user = array();
 
-            $now = current_time( 'mysql' ); 
+            $cur_user['user_login'] = "Research.";
+            $cur_user['user_pass'] = wp_generate_password( 49, false, false );
+            $cur_user['user_email'] = $_POST['em'];
 
-            $later = date( 'Y-m-d H:i:s', strtotime( $now ) + 7200 ); //7200 seconds = 2 hours
-           
-            $add_key_meta = add_user_meta( $user_id->id, 'key_expiry', $later );
-            
-            if ($add_key_meta < 0 ) {
+            $cur_user['user_nicename'] = ""; //user post url
+            $cur_user['user_url'] = ""; //referral url
 
-                return false;
+            $cur_user['first_name'] = $_POST['fn'];
+            $cur_user['last_name'] = $_POST['ln'];
+            $cur_user['display_name'] = $cur_user['first_name'] ." ". $cur_user['last_name'];
 
-            }else{
+            $cur_user['gender'] = $_POST['gd']; //Male, Female
+            $cur_user['birthday'] = $_POST['bd']; //Y-m-d
 
-                return true;
+            $cur_user['country'] = $_POST['gd'];
+            $cur_user['province'] = $_POST['pv'];
+            $cur_user['city'] = $_POST['ct'];
+            $cur_user['brgy'] = $_POST['bg'];
 
-            }
+            $cur_user['show_admin_bar_front'] = false;
+            $cur_user['role'] = "subscriber";
+            $cur_user['user_activation_key'] = wp_generate_password( 12, false, false );
+            $cur_user['user_registered'] = Date("Y-m-d H:i:s");
 
+            return  $cur_user;
         }
 
-        // user create
-        public static function user_create($username,  $email){
-            $user_login = wp_slash( $username );
-            $user_email = wp_slash( $email );
-            $userdata = compact( 'user_login', 'user_email' );
-            return wp_insert_user( $userdata );
-            
-        }
-
-        public static function signup($user_table_name, $data){
-            global $wpdb;
-            return $wpdb->insert($user_table_name, $data);
+        // Try to Send email for a new verification or activation key.
+        public static function is_success_sendmail($user) {
+            $message = "Hello " .$user['display_name']. ",";
+            $message .= "\n\nWelcome to PasaBuy.App! We're happy that your here.";
+            $message .= "\nPassword Reset Key: " .$user['user_activation_key'];
+            $message .= "\n\nPasaBuy.App";
+            $message .= "\nsupport@pasabuy.app";
+            return wp_mail( $user['user_email'], "Bytes Crafter - Forgot Password", $message );
         }
 
     }
