ef03db21ad1b258042fbdfd8e0e6420d5212f50d
Miguel Radaza <miguel1.radaza@gmail.com>
2020-09-08 11:09:15 +0800

    Fixed config pasword expiry span to 1800 seconds means 30 mins

--- includes/source/common/config.php
+++ includes/source/common/config.php
@@ -20,11 +20,11 @@
 	('Maximum image file size', 'Length of Maximum file size to be upload to server.', 'max_img_size', '5', sha2(5, 256));";
 
 	$date = date("Y-m-d h:i:s");
-	
+
 	$dv_config_val = "
 	('configs', 'lock_expiry_span', '1800', '1', '$date', '1', sha2(1, 256) ),
 	('configs', 'master_key', 'datavice', '1', '$date', '2', sha2(2, 256) ),
-	('configs', 'pword_expiry_span', '30', '1', '$date', '3', sha2(3, 256) ),
+	('configs', 'pword_expiry_span', '1800', '1', '$date', '3', sha2(3, 256) ),
 	('configs', 'pword_resetkey_length', '12', '1', '$date', '4', sha2(4, 256) ),
 	('configs', 'max_img_size', '5000000', '1', '$date', '5', sha2(5, 256) )
 	;";
