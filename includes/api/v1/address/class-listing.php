245d770ad7c036c5a6490a0872f9bd6f2b5fb860
Miguel Radaza <miguel1.radaza@gmail.com>
2020-09-09 23:37:12 +0800

    Remove no results found in lsiting address

--- includes/api/v1/address/class-listing.php
+++ includes/api/v1/address/class-listing.php
@@ -4,7 +4,7 @@
 		exit;
 	}
 
-	/** 
+	/**
         * @package datavice-wp-plugin
 		* @version 0.1.0
 		* This is the primary gateway of all the rest api request.
@@ -13,10 +13,10 @@
     class DV_Select_All_Address{
         public static function listen(){
 			global $wpdb;
-			
+
             // Step 1: Validate user
             if ( DV_Verification::is_verified() == false ) {
-                return rest_ensure_response( 
+                return rest_ensure_response(
                     array(
                         "status" => "unknown",
                         "message" => "Please contact your administrator. Verification Issue!",
@@ -26,7 +26,7 @@
 
 			$dv_rev_table = DV_REVS_TABLE;
             $table_address = DV_ADDRESS_TABLE;
-            
+
             $country_table = DV_COUNTRY_TABLE;
             $province_table = DV_PROVINCE_TABLE;
             $city_table = DV_CITY_TABLE;
@@ -45,25 +45,17 @@
 					(SELECT $country_table.country_name FROM $country_table WHERE $country_table.ID = (SELECT dv_rev.child_val FROM $dv_rev_table dv_rev WHERE dv_rev.ID = dv_add.country ) ) as country
 				FROM
 					$table_address dv_add
-				INNER JOIN $dv_rev_table dv_rev 
+				INNER JOIN $dv_rev_table dv_rev
 					ON dv_rev.ID = dv_add.status
 				WHERE (SELECT dv_rev.child_val FROM $dv_rev_table dv_rev WHERE dv_rev.ID = dv_add.status ) = 1 AND dv_add.wpid = $user");
 
-			if (!$result) {
-                return rest_ensure_response( 
-                    array(
-                        "status" => "failed",
-                        "message" => "Address return empty."
-                    )
-                );
-            } else {
-                return rest_ensure_response( 
+
+                return rest_ensure_response(
 					array(
 						"status" => "success",
 						"data" => $result
 					)
 				);
-            }
 
 
 		}
