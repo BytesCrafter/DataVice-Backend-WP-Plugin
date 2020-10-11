67543a410877500d4a8380f0980e43cd3569e636
Miguel Radaza <miguel1.radaza@gmail.com>
2020-10-11 14:12:50 +0800

    added search user

--- includes/api/routes.php
+++ includes/api/routes.php
@@ -26,6 +26,7 @@
     require plugin_dir_path(__FILE__) . '/v1/users/class_verify_activation.php';
     require plugin_dir_path(__FILE__) . '/v1/users/class-link-account.php';
     require plugin_dir_path(__FILE__) . '/v1/users/class-delete-link-acount.php';
+    require plugin_dir_path(__FILE__) . '/v1/users/class-search.php';
     // documents folder
     require plugin_dir_path(__FILE__) . '/v1/users/documents/class-insert.php';
     require plugin_dir_path(__FILE__) . '/v1/users/documents/class-listing.php';
@@ -88,6 +89,12 @@
         /*
          * USER RESTAPI
         */
+
+
+            register_rest_route( 'datavice/v1/user', 'search', array(
+                'methods' => 'POST',
+                'callback' => array('DV_Search_User', 'listen'),
+            ));
 
             register_rest_route( 'datavice/v1/user/account/link', 'delete', array(
                 'methods' => 'POST',
