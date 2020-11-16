86259b082e7057f73c9b9a1aab5688ed2df7e3f9
BytesCrafter <bytescrafter@gmail.com>
2020-11-16 12:44:36 +0800

    Added redirection params on activation.

--- includes/view/blocks/activate.php
+++ includes/view/blocks/activate.php
@@ -57,7 +57,7 @@
 
             return indexed_array;
         }
-
+        
         $("#activation-form").submit(function (e) {
             e.preventDefault();
 
@@ -81,9 +81,18 @@
                 data: formDataActivate,
                 url:'<?= site_url() ?>/wp-json/datavice/v1/user/activate/verify',
                 success:function(response){
-                    response.status!="success"? alert(response.message) : alert("You're account is now activated!");
                     if(response.status == "success") {
-                        $('#activation-form').trigger("reset");
+                        <?php 
+
+                            if( isset($_GET['redirect']) ) {
+                                ?>
+                        window.location.href = "<?= $_GET['redirect'] ?>";
+                                <?php
+                            }
+
+                        ?>
+                    } else {
+                        alert(response.message);
                     }
                     $inputs.prop("disabled", false);
                 },
