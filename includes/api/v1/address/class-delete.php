7a2cb3ae5a117d1b7577617ddac88c92ed70c434
Miguel Radaza <miguel1.radaza@gmail.com>
2020-11-23 22:34:08 -0800

    Revised delete address

--- includes/api/v1/address/class-delete.php
+++ includes/api/v1/address/class-delete.php
@@ -12,100 +12,102 @@
 
     class DV_Delete_Address {
 
+        // Return of Insert user address object from POST.
+        public static function catch_post()
+        {
+            $cur_user = array();
+
+            $cur_user['adid'] = $_POST['adid'];
+            $cur_user['created_by'] = $_POST['wpid'];
+
+            return  $cur_user;
+        }
+
         public static function listen(){
             global $wpdb;
+            $tbl_contacts = DV_CONTACTS_TABLE;
+            $tbl_contacts_filed = DV_CONTACTS_FILEDS;
+            $tbl_brgy = DV_BRGY_TABLE;
+            $tbl_city = DV_CITY_TABLE;
+            $tb_countries = DV_COUNTRY_TABLE;
+            $tbl_province = DV_PROVINCE_TABLE;
 
-               // Step1: Validate user
-               if ( DV_Verification::is_verified() == false ) {
-                return rest_ensure_response(
-                    array(
-                        "status" => "unknown",
-                        "message" => "Please contact your administrator. Verification Issue!",
-                    )
+            // Step1: Validate user
+             if ( DV_Verification::is_verified() == false ) {
+                return array(
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. Verification Issue!",
                 );
             }
 
             // Step 1 : Check if the fields are passed
-            if( !isset($_POST['id'])){
-                return rest_ensure_response(
-                    array(
-                            "status" => "unknown",
-                            "message" => "Please contact your administrator. Request Unknown!",
-                    )
+            if( !isset($_POST['adid'])){
+                return array(
+                    "status"  => "unknown",
+                    "message" => "Please contact your administrator. Request unknown!",
                 );
             }
 
              // Step 1 : Check if the fields are passed
-             if( empty($_POST['id']) ){
-                return rest_ensure_response(
-                    array(
-                            "status" => "unknown",
-                            "message" => "Please contact your administrator. Request Empty!",
-                    )
+             if( empty($_POST['adid'])){
+                return array(
+                    "status"  => "unknown",
+                    "message" => "Please contact your administrator. Request Empty!",
                 );
             }
 
-            // Step5: Check if ID is in valid format (integer)
-            if( !is_numeric($_POST['id']) ){
-                return rest_ensure_response(
-                    array(
-                            "status" => "unknown",
-                            "message" => "Please contact your administrator. Request not in valid format!",
-                    )
-                );
-            }
-
-
-            $table_address = DV_ADDRESS_TABLE;
-            $dv_rev_table = DV_REVS_TABLE;
-            $rev_fields = DV_INSERT_REV_FIELDS;
-            $created_by = $_POST['wpid'];
-            $address_id = $_POST['id'];
-            $date = DV_Globals:: date_stamp();
-
-            // Select last address information using address ID
-            $last_add_data =  $wpdb->get_row("SELECT * FROM $table_address WHERE id = $address_id AND wpid = $created_by ", OBJECT);
-
-            if (!$last_add_data ) {
-                return rest_ensure_response(
-                    array(
-                        "status" => "error",
-                        "message" => "An error while validating addres."
-                    )
-                );
-            }
-
-            //Start for mysql transaction - This is crucial in inserting data with connection with each other
+            // Get user object.
+            $user = self::catch_post();
+            $created_id = $user['created_by'];
             $wpdb->query("START TRANSACTION");
 
-            $wpdb->query("INSERT INTO $dv_rev_table (parent_id, $rev_fields) VALUES ('$address_id', 'address', 'status', '0', $created_by, '$date');");
-            $child_item = $wpdb->insert_id;
 
-            $wpdb->query("UPDATE $table_address SET `status` = '$child_item' WHERE ID = '{$last_add_data->ID}' ");
-            $parent_item = $wpdb->insert_id;
+			$data = DV_Address_Config::get_address(   null,   null,  null, $_POST['adid'] );
+
+            if ($data["status"] == "failed") {
+                return array(
+                    "status" => "failed",
+                    "message" => $data["message"]
+                );
+            }
+
+            $address = array(
+                "st" =>  $data["data"][0]->street,
+                "bg" => DV_Address_Config::get_geo_location( $tbl_brgy, 'brgy_name', $data["data"][0]->brgy )['data'][0]->ID,
+                "ct" => DV_Address_Config::get_geo_location( $tbl_city, 'city_name', $data["data"][0]->city )['data'][0]->city_code,
+                "pv" => DV_Address_Config::get_geo_location( $tbl_province, 'prov_name', $data["data"][0]->province )['data'][0]->prov_code,
+                "co" =>  DV_Address_Config::get_geo_location( $tb_countries, 'country_name', $data["data"][0]->country )['data'][0]->country_code,
+                "type" =>  $data["data"][0]->types
+            );
+
+            $address = DV_Address_Config::add_address( $address, $data["data"][0]->wpid, $data["data"][0]->stid, $data["data"][0]->latitude, $data["data"][0]->longitude,  $data["data"][0]->img_url, 'inactive', $user["adid"]);
+
+            $contact = $wpdb->query("INSERT INTO $tbl_contacts
+                    (`hash_id`, $tbl_contacts_filed, `contact_person`, `status`)
+                VALUES
+                    ('{$data["data"][0]->contact_id}', '{$user["adid"]}', '{$data["data"][0]->contact}', '{$data["data"][0]->contact_type}','$created_id', '{$data["data"][0]->contact_person}', 'inactive') ");
+            $contact_id = $wpdb->insert_id;
+            // End
 
             //Check if any of the insert queries above failed
-            if ( $child_item < 1 || $parent_item < 1 ) {
+            if ( $contact < 1 || $address["status"] == "failed" ) {
 
                 //If failed, do mysql rollback (discard the insert queries(no inserted data))
                 $wpdb->query("ROLLBACK");
 
-                return rest_ensure_response(
-                    array(
-                        "status" => "error",
-                        "message" => "Address failed to updated."
-                    )
+                return array(
+                    "status" => "error",
+                    "message" => "An error occured while submitting data to the server."
                 );
             }
 
             //If no problems found in queries above, do mysql commit (do changes(insert rows))
             $wpdb->query("COMMIT");
 
-            return rest_ensure_response(
-                array(
-                    "status" => "success",
-                    "message" => "Address had been deactivated successfully."
-                )
+            return array(
+                "status" => "success",
+                "message" => "Address Deleted successfully."
             );
-        }
+
+        } // End of listen function
     }
